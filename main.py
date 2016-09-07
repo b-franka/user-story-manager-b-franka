67f9ec4a81355424d2064ccf211dcbbc6b9ab88b
b-franka <b.franku@gmail.com>
2016-09-07 23:52:04 +0200

    story/id, story and list pages work

--- main.py
+++ main.py
@@ -5,38 +5,67 @@ from stories import *
 
 app = Flask(__name__)
 
-@app.route('/story', methods=["GET", "POST"])
-def story_adding():
-    if request.method == "POST":
-        Story.create(story_title=request.form["story_title"],
-                     user_story=request.form["user_story"],
-                     acceptance_criteria=request.form["acceptance_criteria"],
-                     business_value=request.form["business_value"],
-                     estimation=request.form["estimation"],
-                     status=request.form["status"])
-        return redirect(url_for("story_listing"))
-    else:
-        return render_template('form.html', status='new')
-
-
-@app.route('/story/<int:story_id>', methods=["GET", "POST"])
-def story_editing():
-    if request.method == "POST":
-        Story.update(story_title=request.form["story_title"],
-                     user_story=request.form["user_story"],
-                     acceptance_criteria=request.form["acceptance_criteria"],
-                     business_value=request.form["business_value"],
-                     estimation=request.form["estimation"],
-                     status=request.form["status"]).where(Story.id==story_id)
-    else:
-        return render_template('form.html', status='edit')
 
 @app.route('/')
-@app.route('/list', methods=["GET", "POST"])
+@app.route('/list', methods=["GET"])
 def story_listing():
-    # if request.method == "POST":
     stories = Story.select()
     return render_template('list.html', stories=stories)
+
+
+@app.route('/story', methods=["GET"])
+def story_adding():
+    return render_template('form.html', status='new')
+
+
+@app.route('/story', methods=["POST"])
+def story_saving():
+    Story.create(story_title=request.form["story_title"],
+                 user_story=request.form["user_story"],
+                 acceptance_criteria=request.form["acceptance_criteria"],
+                 business_value=request.form["business_value"],
+                 estimation=request.form["estimation"],
+                 status=request.form["status"])
+    return redirect(url_for("story_listing"))
+
+
+@app.route('/story/<int:story_id>', methods=["GET"])
+def story_view(story_id):
+    story = Story.get(Story.id == story_id)
+    return render_template('form.html', status='edit', data=story)
+
+
+@app.route('/story/<int:story_id>', methods=["POST"])
+def story_editing(story_id):
+    if request.method == "POST":
+        story = Story.update(story_title=request.form["story_title"],
+                             user_story=request.form["user_story"],
+                             acceptance_criteria=request.form["acceptance_criteria"],
+                             business_value=request.form["business_value"],
+                             estimation=request.form["estimation"],
+                             status=request.form["status"]).where(Story.id==story_id)
+        story.save()
+    return redirect(url_for("story_listing"))
+
+
+# @app.route('/story/<int:story_id>', methods=["GET", "POST"])
+# def story_editing():
+#     if request.method == "POST":
+#         Story.update(story_title=request.form["story_title"],
+#                      user_story=request.form["user_story"],
+#                      acceptance_criteria=request.form["acceptance_criteria"],
+#                      business_value=request.form["business_value"],
+#                      estimation=request.form["estimation"],
+#                      status=request.form["status"]).where(Story.id==story_id)
+#     else:
+#         return render_template('form.html', status='edit')
+
+# @app.route('/')
+# @app.route('/list', methods=["GET", "POST"])
+# def story_listing():
+#     # if request.method == "POST":
+#     stories = Story.select()
+#     return render_template('list.html', stories=stories)
 
 if __name__ == '__main__':
     app.run(debug=True)
